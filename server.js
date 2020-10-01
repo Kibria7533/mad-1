e6bc44431d1fdaab27730c34fd50ce3e347fc384
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-10-01 18:52:03 +0600

    first commit

--- server.js
+++ server.js
@@ -56,9 +56,9 @@ const startApp = async () => {
 
     if(process.env.NODE_ENV==='production'){
       app.use(express.static('client/build'));
-      // app.get('*',(req,res)=>{
-      //   res.sendFile(path.join('client','build','index.html'));
-      // })
+      app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      })
     }
     // Start Listenting for the server on PORT
     
